4fb9239addee4fe25349288498f920f9ba2f8383
Bri'Anna Tolliver <bri39annat@gmail.com>
2022-12-17 20:34:43 -0600

    finished week 8

--- server/app.js
+++ server/app.js
@@ -1,6 +1,8 @@
 // 'Import' the Express module instead of http
 const express = require("express");
 const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const pizzas = require("./routers/pizzas");
 
 dotenv.config();
 
@@ -8,6 +10,9 @@ const PORT = process.env.PORT || 4040; // we use || to provide a default value
 
 // Initialize the Express application
 const app = express();
+
+mongoose.connect(process.env.MONGODB);
+const db = mongoose.connection;
 
 // Handle the request with HTTP GET method from http://localhost:4040/status
 // app.get("/status", (request, response) => {
@@ -22,6 +27,12 @@ const logging = (request, response, next) => {
 };
 app.use(express.json());
 app.use(logging);
+
+db.on("error", console.error.bind(console, "Connection Error:"));
+db.once(
+  "open",
+  console.log.bind(console, "Successfully opened connection to Mongo!")
+);
 
 app.get("/status", (request, response) => {
   response.status(200).json({ message: "Service healthy" });
@@ -50,6 +61,8 @@ app.post("/add", (request, response) => {
   };
   response.json(responseBody);
 });
+
+app.use("/pizzas", pizzas);
 
 // Tell the Express app to start listening
 // Let the humans know I am running and listening on 4040
